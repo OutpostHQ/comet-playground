387e7180a734444b9bb8880bd67961422f0d43e7
taranbeer <staranbeer@gmail.com>
2023-07-24 10:54:58 +0530

    created sidebar layout

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 
 import Header from "@/components/sections/header"
+import Sidebar from "@/components/sections/sidebar"
 
 export default function Page() {
   return (
     <div>
       <Header />
+      <Sidebar />
     </div>
   )
 }
---
+++ components/sections/sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Sidebar() {
+  return (
+    <aside className="fixed bottom-6 right-6 top-[calc(var(--header-height)+24px)] w-[var(--sidebar-width)] rounded-lg border border-border-default bg-surface-default shadow-cp-shadow-0.25">
+      sidebar
+    </aside>
+  )
+}
