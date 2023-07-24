03e92ea7b3f63f8ba8c746b0bcc2d7cedb08f919
taranbeer <staranbeer@gmail.com>
2023-07-24 11:00:13 +0530

    created the main container

--- app/playground/layout.tsx
+++ app/playground/layout.tsx
@@ -1,5 +1,16 @@
 import React from "react"
 
+import Header from "@/components/sections/header"
+import Sidebar from "@/components/sections/sidebar"
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <main className="min-h-screen bg-surface-subdued pr-[calc(var(--sidebar-width)+24px+24px)]">
+        {children}
+      </main>
+    </div>
+  )
 }
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,13 +1,5 @@
 import React from "react"
 
-import Header from "@/components/sections/header"
-import Sidebar from "@/components/sections/sidebar"
-
 export default function Page() {
-  return (
-    <div>
-      <Header />
-      <Sidebar />
-    </div>
-  )
+  return <div></div>
 }
