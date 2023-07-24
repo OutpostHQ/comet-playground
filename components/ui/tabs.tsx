2b348534f5011b8a62aef408fcc4fb7882569268
taranbeer <staranbeer@gmail.com>
2023-07-24 15:52:36 +0530

    fix border issues on the tabs component

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      " inline-flex items-center justify-center  whitespace-nowrap rounded-none border-b-4 border-b-surface-subdued p-4 text-sm font-medium text-foreground-soft transition-all   data-[state=active]:border-b-surface-primary data-[state=active]:text-foreground-default disabled:pointer-events-none  disabled:opacity-50",
+      " inline-flex items-center justify-center  whitespace-nowrap rounded-none border-b-4 border-b-surface-default p-4 text-sm font-medium text-foreground-soft transition-all   data-[state=active]:border-b-surface-primary data-[state=active]:text-foreground-default disabled:pointer-events-none  disabled:opacity-50",
       className
     )}
     {...props}
