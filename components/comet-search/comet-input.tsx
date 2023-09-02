440196f0a20917d3566686adcdba6c0c75487d26
taranbeer <staranbeer@gmail.com>
2023-09-02 19:27:00 +0530

    update input design

--- components/comet-search/comet-input.tsx
+++ components/comet-search/comet-input.tsx
@@ -4,7 +4,11 @@ export function CometInput() {
   return (
     <div className=" border-t bg-default shadow-1">
       <div className="mx-auto h-32 max-w-[700px]  pt-5 ">
-        <Input className="text-bodyLg" placeholder="Ask a question" autoFocus />
+        <Input
+          className="placeholder:text-default/60 bg-hovered text-bodyLg "
+          placeholder="Ask a question"
+          autoFocus
+        />
       </div>
     </div>
   )
